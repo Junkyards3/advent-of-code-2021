545dd1312320090e5c273393588986262487368e
hf_junk <thehwanderer@gmail.com>
2021-12-15 21:20:03 +0100

    day15 finally done

--- day15.hs
+++ day15.hs
@@ -1,10 +1,11 @@
+{-# OPTIONS_GHC -Wno-incomplete-patterns #-}
 import System.IO ( openFile, hGetContents, IOMode(ReadMode) )
 import qualified Data.Map as Map
 import qualified Data.Set as Set
 import Data.Char (digitToInt)
 import Debug.Trace
-import Data.Array.ST
 import Data.Array
+import Data.Maybe (fromJust)
 
 type Coordinates = (Int,Int)
 type Cavern = Array Coordinates Int
@@ -28,6 +29,7 @@ main = do
     putStrLn "Result for part 1 : " ;
     print $ distance t n (1,1) (n,n);
     putStrLn "Result for part 2 : " ;
+    print $ distance (expand t) (5*n) (1,1) (5*n,5*n);
 
 process :: String -> (Cavern,Int)
 process s = (array ((1,1),(n,n)) [(\ (a, b) -> ((a + 1, b + 1), digitToInt $ (ls !! a) !! b))
@@ -41,17 +43,18 @@ adjacent (a,b) n = filter (\(x,y) -> x >= 1 && y >= 1 && x <= n && y <= n)
     [(a-1,b),(a+1,b),(a,b-1),(a,b+1)]
 
 distance :: Cavern -> Int -> Coordinates -> Coordinates -> InfInt
-distance cav n (ae,be) (as,bs) =  dij (Map.fromList ([ ((i, j), if (i,j) == (ae,be) then Fin 0 else Infinity) | 
-    i <- [1 .. n], j <- [1..n]])) Map.! (as,bs) where
+distance cav lg (ae,be) (as,bs) = dij (Map.fromList [ (if (i,j) == (ae,be) then Fin 0 else Infinity,Set.singleton (i,j)) |
+    i <- [1..lg], j <- [1..lg]]) Set.empty where
 
-    maj :: STUArray Coordinates InfInt -> Coordinates -> Map.Map Coordinates InfInt
-    maj m c = foldr (\c' m' -> Map.insert c' (min (m Map.! c') (m Map.! c `add` Fin (cav ! c'))) m') m (adjacent c n)
+    dij :: Map.Map InfInt (Set.Set Coordinates) -> Set.Set Coordinates -> InfInt
+    dij valCoor visited
+        | (as,bs) `Set.member` sMin = vMin
+        | otherwise = dij valCoor3 newVisited  where
+            ((vMin,sMin), valCoor2) = fromJust $ Map.minViewWithKey valCoor
+            expanded = Set.difference (Set.fromList $ concatMap (`adjacent` lg) sMin) visited
+            newVisited = Set.union visited sMin
+            valCoor3 = foldl (\m (a,b) -> Map.insertWith Set.union (vMin `add` Fin (cav ! (a,b))) (Set.singleton (a,b)) m) valCoor2 expanded
 
-    dij m = case dij' (m,Map.keysSet m) of
-            (m',_) -> m'
-
-    dij' :: (Map.Map Coordinates InfInt, Set.Set Coordinates) -> (Map.Map Coordinates InfInt, Set.Set Coordinates)
-    dij' (m,s)
-        | sMin == (as,bs) = (m,s)
-        | trace (show sMin) otherwise = dij' (maj m sMin, Set.delete sMin s) where
-            sMin = Set.foldr (\newS sMin -> if m Map.! newS < m Map.! sMin then newS else sMin) (Set.elemAt 0 s) s
+expand :: Cavern -> Cavern 
+expand t = array ((1,1),(5*b3,5*b4)) [((x+b3*i,y+b4*j),(ii + (i+j) - 1) `mod` 9 + 1) | i <- [0..4], j <- [0..4], ((x,y),ii) <- assocs t] where 
+    ((b1,b2),(b3,b4)) = bounds t
